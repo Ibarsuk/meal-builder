65e06dbf9bb8b7edf9b65c110364db7a30eec7f5
Ibarsuk <64261965+Ibarsuk@users.noreply.github.com>
2023-02-23 23:05:20 +0300

    edit activity layout

---
+++ app/src/main/java/com/example/meal_builder/MealEdit.java
@@ -0,0 +1,14 @@
+package com.example.meal_builder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MealEdit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meal_edit);
+    }
+}
