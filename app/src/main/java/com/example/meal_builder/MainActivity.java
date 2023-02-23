ec23da37e116533965a6fab53bb052b9f8069541
Ibarsuk <64261965+Ibarsuk@users.noreply.github.com>
2023-02-24 00:55:21 +0300

    title editing

--- app/src/main/java/com/example/meal_builder/MainActivity.java
+++ app/src/main/java/com/example/meal_builder/MainActivity.java
@@ -1,20 +1,37 @@
 package com.example.meal_builder;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.meal_builder.databinding.ActivityMainBinding;
+import com.example.meal_builder.databinding.MealCardExampleBinding;
 import com.example.meal_builder.databinding.MealCardTemplateBinding;
 
 public class MainActivity extends AppCompatActivity {
     private final String TAG = this.getClass().getSimpleName();
+
+    ActivityResultLauncher<Intent> EditLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() != RESULT_OK) {
+                    return;
+                }
+                TextView title = (TextView) findViewById(R.id.card_example_title);
+                title.setText(result.getData().getExtras().get("edited-title").toString());
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
         ImageView cardImg = (ImageView) findViewById(R.id.card_example_img);
         cardImg.setImageResource(R.drawable.breakfast1);
 
-        EditText cardTitle = (EditText) findViewById(R.id.card_example_title);
+        TextView cardTitle = (TextView) findViewById(R.id.card_example_title);
         cardTitle.setText("Мой завтрак");
 
 
@@ -86,5 +103,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void onExampleCardClick(View view) {
         Log.i(TAG, "Add meal btn pressed (declaratively processed)");
+
+        Intent intent = new Intent(this, MealEdit.class);
+        MealCardExampleBinding binding = MealCardExampleBinding.bind(view);
+        intent.putExtra("meal-title", binding.cardExampleTitle.getText().toString());
+
+        EditLauncher.launch(intent);
     }
 }
